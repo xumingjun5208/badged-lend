c4a1adbe1cfeff8eb81d79a0119f252b73576dd6
xumingjun5208 <145756059+xumingjun5208@users.noreply.github.com>
2023-09-25 09:41:05 +0800

    Add files via upload

---
+++ BertLSTM.py
@@ -0,0 +1,63 @@
+# coding: UTF-8
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from transformers import BertModel,BertTokenizer
+
+
+class Config(object):
+
+    """配置参数"""
+    def __init__(self, dataset):
+        self.model_name = 'bert'
+        self.train_path = dataset + '/data/train.txt'                                # 训练集
+        self.dev_path = dataset + '/data/dev.txt'                                    # 验证集
+        self.test_path = dataset + '/data/test.txt'                                  # 测试集
+        self.class_list = [x.strip() for x in open(
+            dataset + '/data/class.txt').readlines()]                                # 类别名单
+        self.save_path = dataset + '/saved_dict/' + self.model_name + '.ckpt'        # 模型训练结果
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')   # 设备
+
+        self.require_improvement = 1000                                 # 若超过1000batch效果还没提升，则提前结束训练
+        self.num_classes = len(self.class_list)                         # 类别数
+        self.num_epochs = 3                                             # epoch数
+        self.batch_size = 128                                           # mini-batch大小
+        self.pad_size = 32                                              # 每句话处理成的长度(短填长切)
+        self.learning_rate = 5e-5                                       # 学习率
+        self.bert_path = './bert_pretrain'
+        self.tokenizer = BertTokenizer.from_pretrained(self.bert_path)
+        self.hidden_size = 768
+        self.filter_sizes = (2, 3, 4)                                   # 卷积核尺寸
+        self.num_filters = 256                                          # 卷积核数量(channels数)
+        self.dropout = 0.1
+        self.rnn_hidden = 768
+        self.num_layers = 2
+
+
+class bertlstm(nn.Module):
+
+    def __init__(self, path):
+        super(bertlstm, self).__init__()
+        self.bert = BertModel.from_pretrained(path)
+        self.hidden_size = 768
+        self.rnn_hidden = 768
+        self.num_layers = 2
+        self.dropout = 0.1
+        self.num_classes=1#er fen lei
+        for param in self.bert.parameters():
+            param.requires_grad = True
+        self.lstm = nn.LSTM(self.hidden_size, self.rnn_hidden, self.num_layers,
+                            bidirectional=True, batch_first=True, dropout=self.dropout)
+        self.dropout = nn.Dropout(self.dropout)
+        self.fc_rnn = nn.Linear(self.rnn_hidden * 2, self.num_classes)
+        self.sig = nn.Sigmoid()
+    def forward(self, x):
+        #context = x[0]  # 输入的句子
+        #mask = x[2]  # 对padding部分进行mask，和句子一个size，padding部分用0表示，如：[1, 1, 1, 1, 0, 0]
+        outputs= self.bert(input_ids=x['input_ids'],attention_mask=x['attention_mask'],token_type_ids=x['token_type_ids'])
+        encoder_out = outputs[0]
+        out, _ = self.lstm(encoder_out)
+        out = self.dropout(out)
+        out = self.fc_rnn(out[:, -1, :])  # 句子最后时刻的 hidden state
+        out1 = self.sig(out)
+        return out1
